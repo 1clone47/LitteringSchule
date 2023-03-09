e73c5040c89d0bdbffb7ba6501e578db30357b8c
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-03-09 19:49:23 +0100

    updated 'HelloWorld.vue' + added sth special ;)

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -2,10 +2,13 @@
 export default {
   data () {
     return {
-
+      enableEasterEgg: false
     }
   },
   methods: {
+    triggerEasterEgg () {
+      this.enableEasterEgg = !this.enableEasterEgg
+    },
     checkQuestion1 (answer) {
       if (answer === 'A') {
         alert("Richtig")
@@ -25,10 +28,14 @@ export default {
 
 <template>
   <div class="w-full h-full items-center space-y-12">
-    <div class="text-center mt-4 text-3xl font-mono underline">Littering</div>
+    <div v-if="enableEasterEgg === false" class="text-center mt-4 text-5xl font-mono underline text-white" @mouseover="triggerEasterEgg">Littering</div>
+    <div v-else class="text-center items-center mt-4 text-5xl font-mono underline text-white" @mouseover="triggerEasterEgg">
+      <p class="text-white text-5xl underline">GOTTCHA!</p>
+      <img src="https://cdn.pixabay.com/photo/2013/07/12/19/33/emperor-penguin-154991_960_720.png">
+    </div>
     <div class="flex flex-col text-center space-y-4 mx-32">
-      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2 hover:">
-        <p>Was verstehst du unter "Littering"?</p>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Was verstehst du unter "Littering"?</p>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
           <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300" @click="checkQuestion1('A')">A: Unangemessene Entsorgung von Abfällen 🗑️ +</button>
           <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300" @click="checkQuestion1('B')">B: Einmaliges benutzen von Mehrwegprodukten</button>
@@ -38,81 +45,81 @@ export default {
           <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300" @click="checkQuestion1('D')">D: Achtlos Müll aus dem Autofenster bei voller Fahrt zu werfen</button>
         </div>
       </div>
-      <div class="border border-red-500 rounded-md flex flex-col space-y-2 py-2">
-        <p>Sotierst du deinen Müll?</p>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Sotierst du deinen Müll?</p>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">A: Ja</button>
-          <button class="flex-1 border text-center">B: Gelegentlich</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">A: Ja</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">B: Gelegentlich</button>
         </div>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">C: Vielleicht 😇</button>
-          <button class="flex-1 border text-center">D: Nein</button>
-        </div>
-      </div>
-      <div class="border border-red-500 rounded-md flex flex-col space-y-2 py-2">
-        <p>Ist Littering legal?</p>
-        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">A: Nein</button>
-          <button class="flex-1 border text-center">B: Bestimmt</button>
-        </div>
-        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">C: Keine Ahnung</button>
-          <button class="flex-1 border text-center">D: Ja +</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">C: Vielleicht 😇</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">D: Nein</button>
         </div>
       </div>
-      <div class="border border-red-500 rounded-md flex flex-col space-y-2 py-2">
-        <p>Wie hoch können die Geldstrafen ausfallen?</p>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Ist Littering legal?</p>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">A: 60€</button>
-          <button class="flex-1 border text-center">B: 200€ - 1.000€</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">A: Nein</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">B: Bestimmt</button>
         </div>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">C: 400€ - 15.000€ +</button>
-          <button class="flex-1 border text-center">D: 5€ - 10€</button>
-        </div>
-      </div>
-      <div class="border border-red-500 rounded-md flex flex-col space-y-2 py-2">
-        <p>Welchem Biom schadet Littering besonders?</p>
-        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">A: Ozeane +</button>
-          <button class="flex-1 border text-center">B: Wälder</button>
-        </div>
-        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">C: Wüste</button>
-          <button class="flex-1 border text-center">D: Gletscher</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">C: Keine Ahnung</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">D: Ja +</button>
         </div>
       </div>
-      <div class="border border-red-500 rounded-md flex flex-col space-y-2 py-2">
-        <p>Wie viel Grundwasser verschmutzt eine Zigarettenstummel?</p>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Wie hoch können die Geldstrafen ausfallen?</p>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">A: 35l - 55l</button>
-          <button class="flex-1 border text-center">B: 40l - 60l +</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">A: 60€</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">B: 200€ - 1.000€</button>
         </div>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">C: 30l - 50l</button>
-          <button class="flex-1 border text-center">D: 42,56l - 61,91l</button>
-        </div>
-      </div>
-      <div class="border border-red-500 rounded-md flex flex-col space-y-2 py-2">
-        <p>Welche deutsche Stadt ist am meisten vom Littering betroffen?</p>
-        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">A: München</button>
-          <button class="flex-1 border text-center">B: Berlin</button>
-        </div>
-        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">C: Baden-Baden +</button>
-          <button class="flex-1 border text-center">D: Friesland</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">C: 400€ - 15.000€ +</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">D: 5€ - 10€</button>
         </div>
       </div>
-      <div class="border border-red-500 rounded-md flex-col flex space-y-2 py-2">
-        <p>Wie lange braucht eine Plastikflasche bis es zerfällt?</p>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Welchem Biom schadet Littering besonders?</p>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">A: ~60 Jahre</button>
-          <button class="flex-1 border text-center">B: ~100 Jahre</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">A: Ozeane +</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">B: Wälder</button>
         </div>
         <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
-          <button class="flex-1 border text-center">C: ~250 Jahre</button>
-          <button class="flex-1 border text-center">D: ~400 Jahre +</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">C: Wüste</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">D: Gletscher</button>
+        </div>
+      </div>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Wie viel Grundwasser verschmutzt eine Zigarettenstummel?</p>
+        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">A: 35l - 55l</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">B: 40l - 60l +</button>
+        </div>
+        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">C: 30l - 50l</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">D: 42,56l - 61,91l</button>
+        </div>
+      </div>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Welche deutsche Stadt ist am meisten vom Littering betroffen?</p>
+        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">A: München</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">B: Berlin</button>
+        </div>
+        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">C: Baden-Baden +</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">D: Friesland</button>
+        </div>
+      </div>
+      <div class="border-4 border-blue-900 bg-white rounded-md flex flex-col space-y-2 py-2">
+        <p class="text-xl">Wie lange braucht eine Plastikflasche bis es zerfällt?</p>
+        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">A: ~60 Jahre</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">B: ~100 Jahre</button>
+        </div>
+        <div class="flex flex-row space-x-4 ml-4 mr-4 h-10">
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">C: ~250 Jahre</button>
+          <button class="flex-1 border-2 border-blue-900 rounded-md text-center hover:bg-gray-300">D: ~400 Jahre +</button>
         </div>
       </div>
     </div>
