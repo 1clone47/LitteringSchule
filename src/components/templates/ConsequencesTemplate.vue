e3b65bb0563a8931efb735e3784f3883c30360c0
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-03-17 10:12:06 +0100

    added content in 'ConsequencesTemplate.vue'

--- src/components/templates/ConsequencesTemplate.vue
+++ src/components/templates/ConsequencesTemplate.vue
@@ -1,12 +1,85 @@
 <template>
-  <div class="bg-blue-300 w-screen h-screen items-center">
-    <div class="border-4 border-blue-900 bg-white flex flex-col text-center mt-4 mx-32 rounded-md">
-      <div class="flex flex-row w-full">
-        <div class="flex-1">
-          <p>HIER STEHT EIN TEXT</p>
+  <div class="bg-blue-300 items-center">
+    <div class="flex flex-col text-center mt-4 mx-32 rounded-md space-y-4">
+      <div class="text-center mt-4 text-5xl font-mono underline text-white">
+        FOLGEN
+      </div>
+      <div class="flex flex-row space-x-4 border-4 rounded-md border-white bg-white items-center justify-center text-center">
+        <div class="text-center justify-center items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-32 h-32">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+          </svg>
         </div>
-        <div class="flex-1">
-          <img src="./images/img1.png" alt="">
+        <div class="text-left">
+          <p>Littering führt dazu, dass Abfälle und Müll auf Straßen, Parks, Stränden und anderen öffentlichen Orten liegen bleiben.</p>
+        </div>
+      </div>
+      <div class="flex flex-row w-full space-x-4">
+        <div class="flex-1 flex flex-col border-4 items-center justify-center text-center border-blue-900 bg-white rounded-md">
+          <p>
+            Umweltverschmutzung:
+          </p>
+          <p>
+            Verschmutzung von Boden, Wasser und Luft und Verschlimmerung des Klimakrise.
+          </p>
+        </div>
+        <div class="flex-1 rounded-md border-4 border-blue-900">
+          <img src="https://cdn.pixabay.com/photo/2021/06/04/06/11/beach-6308884_1280.jpg" alt="">
+        </div>
+      </div>
+      <div class="flex flex-row space-x-4">
+        <div class="flex-1 flex flex-col border-4 items-center justify-center text-center border-blue-900 bg-white rounded-md">
+          <p>Schädigung der Tierwelt</p>
+          <p>Tiere können durch Littering (z.B. Plastiktüten, Flaschen) geschädigt werden, indem sie den Müll fressen oder sich darin verfangen. </p>
+        </div>
+        <div class="flex-1 rounded-md border-4 border-blue-900">
+          <img src="https://cdn.pixabay.com/photo/2020/05/31/18/40/fish-5243741_1280.jpg" alt="">
+        </div>
+      </div>
+      <div class="flex flex-row w-full space-x-4">
+        <div class="flex-1 flex flex-col border-4 items-center justify-center text-center border-blue-900 bg-white rounded-md">
+          <p>Gesundheitsrisiken</p>
+          <p>Abfall und Müll können eine Vielzahl von Krankheiten und Infektionen verursachen, da sie Bakterien, Viren und andere schädliche Substanzen enthalten können.</p>
+        </div>
+        <div class="flex-1 rounded-md border-4 border-blue-900">
+          <img src="https://cdn.pixabay.com/photo/2016/11/14/03/05/surgery-1822458_1280.jpg" alt="">
+        </div>
+      </div>
+      <div class="flex flex-row space-x-4">
+        <div class="flex-1 flex flex-col border-4 items-center justify-center text-center border-blue-900 bg-white rounded-md">
+          <p>Ästhetische Beeinträchtigung</p>
+          <p>Littering kann die Schönheit von öffentlichen Plätzen beeinträchtigen und zu einer unansehnlichen und unattraktiven Umgebung führen.</p>
+        </div>
+        <div class="flex-1 rounded-md border-4 border-blue-900">
+          <img src="https://cdn.pixabay.com/photo/2020/03/03/02/26/trash-4897356_1280.jpg" alt="">
+        </div>
+      </div>
+      <div class="flex flex-row space-x-4">
+        <div class="flex-1 flex flex-col border-4 items-center justify-center text-center border-blue-900 bg-white rounded-md">
+          <p class="align-text-top">Wirtschaftliche Kosten</p>
+          <p>Die Beseitigung von Littering kann sehr teuer sein und Steuergelder in Anspruch nehmen, die für andere Zwecke verwendet werden könnten. </p>
+        </div>
+        <div class="flex-1 rounded-md border-4 border-blue-900">
+          <img src="https://cdn.pixabay.com/photo/2016/10/09/19/19/coins-1726618_1280.jpg" alt="">
+        </div>
+      </div>
+      <div class="flex flex-row space-x-4">
+        <div class="flex-1 flex flex-col border-4 items-center justify-center text-center border-blue-900 bg-white rounded-md">
+          <p>Reputationsverlust</p>
+          <p>Littering kann das Ansehen einer Stadt oder Region negativ beeinflussen, was sich auf den Tourismus und die lokale Wirtschaft auswirken kann.</p>
+        </div>
+        <div class="flex-1 rounded-md border-4 border-blue-900">
+          <img src="https://cdn.pixabay.com/photo/2018/11/29/21/19/hamburg-3846525_1280.jpg" alt="">
+        </div>
+      </div>
+      <div class="flex flex-row space-x-4 border-4 rounded-md border-white bg-white items-center justify-center text-center">
+        <div class="text-center justify-center items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-32 h-32">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+          </svg>
+        </div>
+        <div class="text-left">
+          <p>Insgesamt können die Folgen von Littering erheblich sein und sowohl die Umwelt als auch die Gesellschaft in vielfältiger Weise beeinträchtigen.</p>
         </div>
       </div>
     </div>
