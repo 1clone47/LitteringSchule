25839ff5bb5a7c395fcaffca4975afaa35cce2d9
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-03-24 09:24:19 +0100

    fixed responsive for NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,7 +8,6 @@
           py-8
           mx-auto
           md:flex md:justify-between md:items-center
-          xs:flex xs:justify-between xs:items-center
         "
       >
         <div class="flex items-center justify-between">
@@ -20,13 +19,15 @@
               text-gray-800
               md:text-2xl
               xs:text-xl
+              xs:text-left
+              xs:justify-start
               hover:text-blue-400
             "
           >
             Quiz
           </router-link>
           <!-- Mobile menu button -->
-          <div @click="showMenu = !showMenu" class="flex md:hidden xs:hidden">
+          <div @click="showMenu = !showMenu" class="flex md:hidden">
             <button
                 type="button"
                 class="
@@ -35,7 +36,7 @@
                 focus:outline-none focus:text-gray-400
               "
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 fill-current">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z" />
               </svg>
 
@@ -54,15 +55,8 @@
             md:flex
             md:space-y-0
             md:flex-row
-            md:items-center
             md:space-x-10
             md:mt-0
-            xs:flex
-            xs:space-y-0
-            xs:flex-row
-            xs:items-center
-            xs:space-x-10
-            xs:mt-0
           "
         >
           <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
