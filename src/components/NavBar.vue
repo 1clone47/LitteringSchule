a7e6c8b82278118947d99efc014f9d6c7faaf190
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-03-24 09:09:09 +0100

    changed NavBar design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,82 @@
-<script setup>
-import { RouterLink } from 'vue-router'
-</script>
-
 <template>
-  <div class="w-auto">
-    <div class="flex flex-row w-auto h-20 xs:h-16 bg-white items-center justify-center border-b-4 border-blue-900 xs:text-xs bg-white">
-      <RouterLink to="/" class="flex-1 hover:text-blue-900 items-center text-center justify-center xl:font-bold  xs:mr-2">QUIZ</RouterLink>
-      <RouterLink to="/cause"  class="flex-1 hover:text-blue-900 text-center xl:font-bold">URSACHEN</RouterLink>
-      <RouterLink to="/consequences" class="flex-1 hover:text-blue-900 text-center xl:font-bold">FOLGEN</RouterLink>
-      <RouterLink to="/measures" class="flex-1 hover:text-blue-900 text-center xl:font-bold xs:mr-2">MAßNAHMEN</RouterLink>
+  <div>
+    <div class="bg-gray-100 border-b-4 border-blue-900">
+      <nav
+          class="
+          container
+          px-6
+          py-8
+          mx-auto
+          md:flex md:justify-between md:items-center
+        "
+      >
+        <div class="flex items-center justify-between">
+          <router-link
+              to="/"
+              class="
+              text-xl
+              font-bold
+              text-gray-800
+              md:text-2xl
+              hover:text-blue-400
+            "
+          >
+            Quiz
+          </router-link>
+          <!-- Mobile menu button -->
+          <div @click="showMenu = !showMenu" class="flex md:hidden">
+            <button
+                type="button"
+                class="
+                text-gray-800
+                hover:text-gray-400
+                focus:outline-none focus:text-gray-400
+              "
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z" />
+              </svg>
+
+            </button>
+          </div>
+        </div>
+
+
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul
+            :class="showMenu ? 'flex' : 'hidden'"
+            class="
+            flex-col
+            mt-8
+            space-y-4
+            md:flex
+            md:space-y-0
+            md:flex-row
+            md:items-center
+            md:space-x-10
+            md:mt-0
+          "
+        >
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <RouterLink to="/cause">Ursachen</RouterLink>
+          </li>
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <RouterLink to="/consequences">Folgen</RouterLink>
+          </li>
+          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+            <RouterLink to="/measures">Maßnahmen</RouterLink>
+          </li>
+        </ul>
+      </nav>
     </div>
-    </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+};
+</script>
