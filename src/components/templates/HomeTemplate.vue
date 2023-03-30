7df13f7ccbfa52e933d7e54376b7f58219ba2cfd
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-03-30 15:49:02 +0200

    added content to 'HomeTemplate.vue'

--- src/components/templates/HomeTemplate.vue
+++ src/components/templates/HomeTemplate.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="bg-gradient-to-r from-blue-300 via-blue-500 to-blue-300 items-center w-auto h-auto justify-center items-center">
-    <div class="items-center justify-center text-center mx-2 border-4 py-32 px-32 rounded-md mt-4 bg-white border-white text-5xl font-bold">
-      <p>
-        Littering
-      </p>
-    </div>
-    <div class="bg-white flex flex-col items-center mt-36">
-      <div class="text-2xl font-semibold mt-2">
-        <h1>Was ist Littering?</h1>
+    <div  class="text-center mt-4 text-5xl font-sans border-2 mx-32 xs:mx-2 xs:text-2xl rounded-full font-semibold py-2 text-white">Littering</div>
+    <div class="flex flex-row space-x-4 mx-2 xs:mx-2 border-4 rounded-md border-white bg-white items-center justify-center text-center mt-4">
+      <div class="text-center justify-center items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-32 h-32">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M20.893 13.393l-1.135-1.135a2.252 2.252 0 01-.421-.585l-1.08-2.16a.414.414 0 00-.663-.107.827.827 0 01-.812.21l-1.273-.363a.89.89 0 00-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.212.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 01-1.81 1.025 1.055 1.055 0 01-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.655-.261a2.25 2.25 0 01-1.383-2.46l.007-.042a2.25 2.25 0 01.29-.787l.09-.15a2.25 2.25 0 012.37-1.048l1.178.236a1.125 1.125 0 001.302-.795l.208-.73a1.125 1.125 0 00-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 01-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 01-1.458-1.137l1.411-2.353a2.25 2.25 0 00.286-.76m11.928 9.869A9 9 0 008.965 3.525m11.928 9.868A9 9 0 118.965 3.525" />
+        </svg>
       </div>
-      <div class="flex flex-col space-y-4 mt-4 mx-2">
-        <p>
-          Littering bezieht sich auf das unachtsame Wegwerfen von Abfall oder Müll auf öffentlichen Plätzen oder in der Natur. Dies kann zum Beispiel Zigarettenstummel, Plastikflaschen, Papierverpackungen oder Essensreste umfassen.
-        </p>
+      <div class="text-left">
+        <p class="xl:text-3xl xs:text-xs">Beantworte alle Fragen um herauszufinden was wie eine Sorte von Verbraucher du bist.</p>
+      </div>
+    </div>
+    <div class="flex snap-x snap-mandatory h-auto w-auto mx-2 overflow-scroll border-4 rounded-md border-white bg-white mt-4">
+      <div class="snap-start shrink-0 grid w-full place-items-center">
+        <img class="w-2/3 h-2/3 rounded-md" src="https://cdn.pixabay.com/photo/2016/11/21/15/42/disposal-1846033_1280.jpg">
+      </div>
+      <div class="snap-start shrink-0 grid w-full place-items-center">
+        <img class="w-2/3 h-2/3 rounded-md" src="https://cdn.pixabay.com/photo/2018/04/04/11/39/safety-net-3289548_1280.jpg">
+      </div>
+      <div class="snap-start shrink-0 grid w-full place-items-center">
+        <img class="w-2/3 h-2/3 rounded-md" src="https://cdn.pixabay.com/photo/2020/02/17/06/00/pollution-4855507_1280.jpg">
+      </div>
+      <div class="snap-start shrink-0 grid w-full place-items-center">
+        <img class="w-2/3 h-2/3 rounded-md" src="https://cdn.pixabay.com/photo/2020/03/09/04/25/beach-4914403_1280.jpg">
+      </div>
+      <div class="snap-start shrink-0 grid w-full place-items-center">
+        <img class="w-2/3 h-2/3 rounded-md" src="https://cdn.pixabay.com/photo/2014/10/20/12/28/rubbish-495213__480.jpg">
+      </div>
+      <div class="snap-start shrink-0 grid w-full place-items-center">
+        <img class="w-2/3 h-2/3 rounded-md" src="https://cdn.pixabay.com/photo/2014/09/08/15/16/pollution-439188__480.jpg" alt="">
       </div>
     </div>
   </div>
