38e4aa7a6af3c81b494b32e7b1c37ef1141dca23
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-08-31 01:30:11 +0200

    add content to causes

--- components/Causes/causes-items.ts
+++ components/Causes/causes-items.ts
@@ -1,38 +1,47 @@
 export const REASONS = [
     {
         title : 'Mangel an Umweltbewusstsein und Bildung',
-        content : 'Viele Menschen sind nicht ausreichend über die negativen Auswirkungen von Littering informiert. Oft fehlt es an Verständnis dafür, wie Müll in der Umwelt landet, wie er die Tierwelt beeinträchtigt und wie er zu Umweltverschmutzung und Gesundheitsproblemen führen kann. Ein Bildungsmangel in diesem Bereich bedeutet, dass viele Menschen nicht die Verbindung zwischen ihrem individuellen Verhalten und den globalen Umweltauswirkungen herstellen können.'
+        content : 'Viele Menschen sind nicht ausreichend über die negativen Auswirkungen von Littering informiert. Oft fehlt es an Verständnis dafür, wie Müll in der Umwelt landet, wie er die Tierwelt beeinträchtigt und wie er zu Umweltverschmutzung und Gesundheitsproblemen führen kann. Ein Bildungsmangel in diesem Bereich bedeutet, dass viele Menschen nicht die Verbindung zwischen ihrem individuellen Verhalten und den globalen Umweltauswirkungen herstellen können.',
+        image : 'https://cdn.pixabay.com/photo/2015/10/05/18/35/plastic-cups-973103_1280.jpg'
     },
     {
         title : 'Bequemlichkeit',
-        content : 'In der modernen Welt suchen die Menschen oft nach bequemen Lösungen für alltägliche Probleme. Wenn keine Mülleimer in der Nähe sind oder wenn es einfacher erscheint, den Müll einfach auf den Boden fallen zu lassen, als ihn mitzunehmen, steigt die Wahrscheinlichkeit von Littering. Die Suche nach der bequemsten Option führt dazu, dass Menschen unachtsam mit Abfällen umgehen.'
+        content : 'In der modernen Welt suchen die Menschen oft nach bequemen Lösungen für alltägliche Probleme. Wenn keine Mülleimer in der Nähe sind oder wenn es einfacher erscheint, den Müll einfach auf den Boden fallen zu lassen, als ihn mitzunehmen, steigt die Wahrscheinlichkeit von Littering. Die Suche nach der bequemsten Option führt dazu, dass Menschen unachtsam mit Abfällen umgehen.',
+        image : 'https://cdn.pixabay.com/photo/2016/12/14/19/07/man-1907204_1280.jpg'
     },
     {
         title : 'Fehlende Infrastruktur',
-        content : 'Eine unzureichende Anzahl von Mülleimern und Recycling-Behältern in öffentlichen Bereichen kann Menschen dazu veranlassen, ihren Müll unachtsam zu entsorgen. Wenn keine angemessenen Optionen vorhanden sind, kann die Versuchung groß sein, den Müll einfach liegen zu lassen, anstatt ihn mitzunehmen und später zu entsorgen.'
+        content : 'Eine unzureichende Anzahl von Mülleimern und Recycling-Behältern in öffentlichen Bereichen kann Menschen dazu veranlassen, ihren Müll unachtsam zu entsorgen. Wenn keine angemessenen Optionen vorhanden sind, kann die Versuchung groß sein, den Müll einfach liegen zu lassen, anstatt ihn mitzunehmen und später zu entsorgen.',
+        image : 'https://cdn.pixabay.com/photo/2020/07/13/13/20/garbage-5400780_1280.jpg'
     },
     {
         title : 'Soziale Normen und Peer-Pressure',
-        content : 'Das Verhalten anderer Menschen um uns herum kann unsere Handlungen stark beeinflussen. Wenn Littering in einer bestimmten Umgebung als normal oder akzeptabel angesehen wird, könnten Menschen, um sich anzupassen oder nicht herauszustechen, dazu neigen, dasselbe Verhalten zu zeigen. Diese sozialen Normen und der Druck von Gleichaltrigen können dazu führen, dass Menschen Littering als akzeptable Praxis betrachten.'
+        content : 'Das Verhalten anderer Menschen um uns herum kann unsere Handlungen stark beeinflussen. Wenn Littering in einer bestimmten Umgebung als normal oder akzeptabel angesehen wird, könnten Menschen, um sich anzupassen oder nicht herauszustechen, dazu neigen, dasselbe Verhalten zu zeigen. Diese sozialen Normen und der Druck von Gleichaltrigen können dazu führen, dass Menschen Littering als akzeptable Praxis betrachten.',
+        image : 'https://cdn.pixabay.com/photo/2019/03/12/12/47/people-4050698_1280.jpg'
     },
     {
         title : 'Gedankenlosigkeit und Gleichgültigkeit',
-        content : 'In einer Welt, die oft von Geschwindigkeit und Ablenkungen geprägt ist, denken viele Menschen möglicherweise nicht über die langfristigen Konsequenzen ihres Handelns nach. Die Gleichgültigkeit gegenüber Umweltbelangen und die Annahme, dass individuelle Handlungen keinen großen Einfluss haben, können dazu führen, dass Menschen ihren Müll achtlos wegwerfen.'
+        content : 'In einer Welt, die oft von Geschwindigkeit und Ablenkungen geprägt ist, denken viele Menschen möglicherweise nicht über die langfristigen Konsequenzen ihres Handelns nach. Die Gleichgültigkeit gegenüber Umweltbelangen und die Annahme, dass individuelle Handlungen keinen großen Einfluss haben, können dazu führen, dass Menschen ihren Müll achtlos wegwerfen.',
+        image : 'https://cdn.pixabay.com/photo/2019/12/01/12/57/bin-4665444_1280.jpg'
     },
     {
         title : 'Schnelllebige Lebensweise',
-        content : 'In unserer heutigen Gesellschaft haben viele Menschen wenig Zeit und Geduld. Die Suche nach einem Mülleimer kann als zeitaufwändig erscheinen, insbesondere in Momenten, in denen Menschen unter Zeitdruck stehen. Diese schnelle Lebensweise kann dazu führen, dass Menschen ihren Müll einfach auf den Boden werfen, um Zeit zu sparen.'
+        content : 'In unserer heutigen Gesellschaft haben viele Menschen wenig Zeit und Geduld. Die Suche nach einem Mülleimer kann als zeitaufwändig erscheinen, insbesondere in Momenten, in denen Menschen unter Zeitdruck stehen. Diese schnelle Lebensweise kann dazu führen, dass Menschen ihren Müll einfach auf den Boden werfen, um Zeit zu sparen.',
+        image : 'https://cdn.pixabay.com/photo/2019/09/18/06/34/passenger-4485564_1280.jpg'
     },
     {
         title : 'Mangelnde persönliche Verantwortung',
-        content : 'Einige Menschen könnten der Meinung sein, dass es die Aufgabe anderer ist, sich um die Abfallentsorgung zu kümmern. Dies kann zu einem Teufelskreis führen, bei dem niemand die Verantwortung übernimmt. Wenn jeder darauf wartet, dass jemand anderes aufräumt, wird die Umgebung weiter verschmutzt.'
+        content : 'Einige Menschen könnten der Meinung sein, dass es die Aufgabe anderer ist, sich um die Abfallentsorgung zu kümmern. Dies kann zu einem Teufelskreis führen, bei dem niemand die Verantwortung übernimmt. Wenn jeder darauf wartet, dass jemand anderes aufräumt, wird die Umgebung weiter verschmutzt.',
+        image : 'https://cdn.pixabay.com/photo/2018/03/25/09/15/world-3258866_1280.jpg'
     },
     {
         title : 'Werbung und Konsumkultur',
-        content : 'Die Werbung und die Konsumkultur fördern oft den Gedanken, dass neue Produkte besser sind als alte und dass Wegwerfen normal ist. Diese Einstellung beeinflusst das Verhalten der Menschen, indem sie dazu ermutigt werden, Produkte als kurzlebig und ersetzbar zu betrachten. Dies kann dazu führen, dass Menschen Müll als weniger bedeutsam ansehen.'
+        content : 'Die Werbung und die Konsumkultur fördern oft den Gedanken, dass neue Produkte besser sind als alte und dass Wegwerfen normal ist. Diese Einstellung beeinflusst das Verhalten der Menschen, indem sie dazu ermutigt werden, Produkte als kurzlebig und ersetzbar zu betrachten. Dies kann dazu führen, dass Menschen Müll als weniger bedeutsam ansehen.',
+        image : 'https://cdn.pixabay.com/photo/2016/10/09/08/32/digital-marketing-1725340_1280.jpg'
     },
     {
         title : 'Mangelnde Sanktionen',
-        content : 'Wenn es keine spürbaren Konsequenzen für Littering gibt, fehlt möglicherweise der Anreiz, das Verhalten zu ändern. Wenn Menschen das Gefühl haben, dass sie ungestraft Müll wegwerfen können, besteht wenig Motivation, ihr Verhalten zu ändern. Dies kann die Verbreitung von Littering verstärken.'
+        content : 'Wenn es keine spürbaren Konsequenzen für Littering gibt, fehlt möglicherweise der Anreiz, das Verhalten zu ändern. Wenn Menschen das Gefühl haben, dass sie ungestraft Müll wegwerfen können, besteht wenig Motivation, ihr Verhalten zu ändern. Dies kann die Verbreitung von Littering verstärken.',
+        image : 'https://cdn.pixabay.com/photo/2019/02/19/16/38/tax-office-4007153_1280.jpg'
     },
 ]
