fe62bcfea4d6e38b7fe2c2d50e709ff32a940049
anwa <andy_wagner@aikq.de>
2023-09-05 11:44:43 +0200

    fix responsive design

--- components/Causes/index.vue
+++ components/Causes/index.vue
@@ -3,10 +3,10 @@
     <p class="header-font">
       Was sind die Ursachen?
     </p>
-    <p class="text-2xl font-mono xl:mx-80 lg:mx-40 mt-12">
+    <p class="text-2xl font-mono 2xl:mx-80 xl:mx-40 mt-12">
       Die Bekämpfung von Littering erfordert eine umfassende Herangehensweise, die Bildung, Bewusstseinsbildung, angemessene Infrastruktur, rechtliche Durchsetzung und kulturelle Veränderungen umfasst. Nur wenn diese Ursachen angegangen werden, kann eine langfristige Verringerung von Littering erreicht werden.
     </p>
-    <div v-for="reason in REASONS" class="flex lg:flex-col xl:flex-row items-center justify-center xl:mx-80 lg:mx-40 space-y-4 space-x-4 mt-12">
+    <div v-for="reason in REASONS" class="flex lg:flex-col xl:flex-row items-center justify-center 2xl:mx-80 xl:mx-40 space-y-4 space-x-4 mt-12">
       <img :src="reason.image" alt="" class="lg:w-full xl:w-[450px] h-[450px] rounded-lg shadow-2xl ">
       <div class="flex flex-col items-center justify-center py-4">
         <p class="text-xl font-semibold">{{ reason.title }}</p>
--- components/Consequences/index.vue
+++ components/Consequences/index.vue
@@ -3,10 +3,10 @@
       <p class="header-font">
         Was sind die Folgen?
       </p>
-      <p class="text-2xl font-mono xl:mx-80 lg:mx-40 mt-12">
+      <p class="text-2xl font-mono 2xl:mx-80 xl:mx-40 mt-12">
         Die Konsequenzen von Littering sind vielfältig und weitreichend. Sie beeinflussen die Umwelt, die Gesundheit, die Wirtschaft und das soziale Gefüge einer Gesellschaft. Daher ist es entscheidend, das Bewusstsein für diese Konsequenzen zu schärfen und Maßnahmen zu ergreifen, um Littering zu verhindern und zu reduzieren.
       </p>
-      <div v-for="reason in REASONS" class="flex md:flex-col lg:flex-row items-center justify-center xl:mx-80 lg:mx-40 space-y-4 space-x-4 mt-12">
+      <div v-for="reason in REASONS" class="flex md:flex-col lg:flex-row items-center justify-center 2xl:mx-80 xl:mx-40 space-y-4 space-x-4 mt-12">
         <img :src="reason.image" alt="" class="lg:w-full xl:w-[450px] h-[450px] rounded-lg shadow-2xl">
         <div class="flex flex-col items-center justify-center py-4">
           <p class="text-xl font-semibold">{{ reason.title }}</p>
