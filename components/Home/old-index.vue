a4c655b42043a2aff9c798cfbbf394a9aa0505e2
anwa <andy_wagner@aikq.de>
2023-09-06 13:12:34 +0200

    fix responsive design for home

--- components/Home/old-index.vue
+++ components/Home/old-index.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="py-24 px-8 lg:px-32 xl:px-64 flex flex-col items-center">
-    <p class="font-bold text-3xl">
+  <div class="py-24">
+    <p class="header-font flex justify-center">
       Was ist Littering?
     </p>
-    <label for="quiz-button" class="mt-4 mb-4 font-mono text-lg lg:text-xl lg:mb-8 lg:mt-8">
-      ~ Probier unser Quiz aus ~
-    </label>
-    <NuxtLink to="/quiz" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-      <ArrowRightOnRectangleIcon class="w-8 h-8" />
-    </NuxtLink>
-    <div class="flex flex-col lg:flex-row mt-4 lg:mt-8">
-      <div class="flex-1 flex items-center">
-        <img src="~/assets/images/home-image.jpg" class="rounded-lg lg:rotate-90 xl:rotate-0 shadow-xl xs:w-full xs:h-[350px]" alt="">
-      </div>
-      <p class="flex-1 py-2 px-4 flex font-mono items-center">
+    <div class="flex flex-col 2xl:mx-80 xl:mx-40 items-center space-y-4 justify-center mt-12 text-2xl font-mono">
+      <p>~ Probiere das Quiz aus um herauszufinden, zu welcher Kategorie Sie gehoeren ~</p>
+      <NuxtLink to="/quiz" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+        <ArrowRightOnRectangleIcon class="w-8 h-8" />
+      </NuxtLink>
+    </div>
+    <div class="flex flex-col items-center mt-12 2xl:mx-80 xl:mx-40">
+      <img src="~/assets/images/home-image.jpg" class="rounded-lg shadow-2xl mb-2 xl:w-full xl:h-[450px]" alt="">
+      <p class="flex-1 mt-4 flex items-center xl:text-2xl lg:text-sm font-mono">
         Littering bezieht sich auf das unachtsame Wegwerfen von Abfall oder Müll auf öffentlichen Plätzen oder in der Natur.
+        <br />
         Dies kann zum Beispiel Zigarettenstummel, Plastikflaschen, Papierverpackungen oder Essensreste umfassen.
+        <br />
         Littering ist ein weit verbreitetes Problem in vielen Städten und Gemeinden, da es nicht nur unansehnlich ist, sondern auch Umweltprobleme verursacht.
+        <br />
         Wenn Abfälle auf öffentlichen Plätzen liegen gelassen werden, können sie zum Beispiel in Flüsse und Ozeane gelangen, wo sie die Tierwelt und die Umwelt schädigen können.
+        <br />
         Littering ist auch eine Form von Umweltverschmutzung und kann dazu beitragen, dass sich Krankheiten und Infektionen ausbreiten, wenn Abfall in der Nähe von Wohngebieten oder öffentlichen Einrichtungen aufbewahrt wird.
+        <br />
         Es ist wichtig, dass jeder Verantwortung übernimmt und Müll ordnungsgemäß entsorgt, um Littering und seine negativen Auswirkungen auf unsere Umwelt zu reduzieren.
       </p>
     </div>
@@ -26,5 +29,5 @@
 </template>
 
 <script setup>
-import { ArrowRightOnRectangleIcon } from "@heroicons/vue/24/solid";
+import { GlobeEuropeAfricaIcon, ArrowRightOnRectangleIcon } from "@heroicons/vue/24/solid";
 </script>
