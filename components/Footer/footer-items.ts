054d4cdc3cc5c3da2a39ee2f428c1bdb7f0d2226
anwa <andy_wagner@aikq.de>
2023-09-07 13:28:10 +0200

    update footer

--- components/Footer/footer-items.ts
+++ components/Footer/footer-items.ts
@@ -1,14 +1,14 @@
 export const FOOTER = [
     {
-        key : 'start-help',
+        key : 'Hilf mit!',
         url : 'https://www.wwf.de/themen-projekte/plastik?gclid=Cj0KCQiAx6ugBhCcARIsAGNmMbjNAytmhemDjCMIwHT8SgxhXLObUSLric7Wy3lLBuP3Gdc0n8C9TSkaAoJaEALw_wcB'
     },
     {
-        key : 'calculate-footstep',
+        key : 'Fußabdruck berechnen',
         url : 'https://www.sauberenergie.de/engagement/co2-rechner?gclid=Cj0KCQiAx6ugBhCcARIsAGNmMbizjZSLvdAUhzijj-a3ggzb7alPV5OaHqwyFO0asS2sS52qQtd6NxsaAtxzEALw_wcB#2'
     },
     {
-        key : 'news',
+        key : 'Neuigkeiten',
         url : 'https://www.umweltbundesamt.de/tags/littering'
     }
 ]
