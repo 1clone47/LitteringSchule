fdd17616c85e5c9e6e5ac694220bb04e00c4f450
anwa <andy_wagner@aikq.de>
2023-09-21 16:40:01 +0200

    add first design idea

--- components/Home/index.vue
+++ components/Home/index.vue
@@ -1,30 +1,39 @@
 <template>
-  <div class="py-24 px-8 lg:px-32 xl:px-64 flex flex-col items-center">
-    <p class="font-bold text-3xl">
-      Was ist Littering?
-    </p>
-    <label for="quiz-button" class="mt-4 mb-4 font-mono text-lg lg:text-xl lg:mb-8 lg:mt-8">
-      ~ Probier unser Quiz aus ~
-    </label>
-    <NuxtLink to="/quiz" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-      START
-    </NuxtLink>
-    <div class="flex flex-col lg:flex-row mt-4 lg:mt-8">
-      <div class="flex-1 flex items-center">
-        <img src="~/assets/images/home-image.jpg" class="rounded-lg lg:rotate-90 xl:rotate-0 shadow-xl xs:w-full xs:h-[350px]" alt="">
+  <div class="min-h-screen bg-[url('~/assets/images/background.png')] bg-cover bg-center flex flex-col">
+    <div class="flex text-white text-2xl space-x-8 p-4 mb-32">
+      <p>Start</p>
+      <p>Quiz</p>
+      <p>Ursachen</p>
+      <p>Folgen</p>
+      <p>Massnahmen</p>
+    </div>
+    <div class="flex flex-col text-white items-center mt-28 mb-24">
+      <p class="text-[100px] font-bold">LITTERING</p>
+      <p class="text-3xl">Die neue Bedrohung?</p>
+    </div>
+    <div class="text-white flex-col flex items-center mt-24">
+      <p>Scroll um mehr zu erfahren</p>
+      <ChevronDownIcon class="w-28 h-28 text-white animate-bounce p-2" />
+    </div>
+  </div>
+  <div class="min-h-screen bg-[#E7EBF0] flex flex-col py-4 px-8">
+    <div class="flex flex-row space-x-8">
+      <div class="flex flex-col justify-center space-y-3">
+        <p class="font-bold text-4xl">Was ist Littering?</p>
+        <p class="text-2xl">Littering bezieht sich auf das unachtsame Wegwerfen von Abfall oder Müll auf öffentlichen Plätzen oder in der Natur. Dies kann zum Beispiel Zigarettenstummel, Plastikflaschen, Papierverpackungen oder Essensreste umfassen. Littering ist ein weit verbreitetes Problem in vielen Städten und Gemeinden, da es nicht nur unansehnlich ist, sondern auch Umweltprobleme verursacht.</p>
       </div>
-      <p class="flex-1 py-2 px-4 flex font-mono items-center">
-        Littering bezieht sich auf das unachtsame Wegwerfen von Abfall oder Müll auf öffentlichen Plätzen oder in der Natur.
-        Dies kann zum Beispiel Zigarettenstummel, Plastikflaschen, Papierverpackungen oder Essensreste umfassen.
-        Littering ist ein weit verbreitetes Problem in vielen Städten und Gemeinden, da es nicht nur unansehnlich ist, sondern auch Umweltprobleme verursacht.
-        Wenn Abfälle auf öffentlichen Plätzen liegen gelassen werden, können sie zum Beispiel in Flüsse und Ozeane gelangen, wo sie die Tierwelt und die Umwelt schädigen können.
-        Littering ist auch eine Form von Umweltverschmutzung und kann dazu beitragen, dass sich Krankheiten und Infektionen ausbreiten, wenn Abfall in der Nähe von Wohngebieten oder öffentlichen Einrichtungen aufbewahrt wird.
-        Es ist wichtig, dass jeder Verantwortung übernimmt und Müll ordnungsgemäß entsorgt, um Littering und seine negativen Auswirkungen auf unsere Umwelt zu reduzieren.
-      </p>
+      <img src="~/assets/images/Home-Img1.png" class="w-[450px] h-[450px]" alt="">
+    </div>
+    <div class="flex flex-row space-x-8">
+      <img src="~/assets/images/Home-Img2.png" class="w-[450px] h-[450px]" alt="">
+      <div class="flex flex-col justify-center space-y-3">
+        <p class="font-bold text-4xl">Warum eine Gefahr?</p>
+        <p class="text-2xl">Wenn Abfälle auf öffentlichen Plätzen liegen gelassen werden, können sie zum Beispiel in Flüsse und Ozeane gelangen, wo sie die Tierwelt und die Umwelt schädigen können. Littering ist auch eine Form von Umweltverschmutzung und kann dazu beitragen, dass sich Krankheiten und Infektionen ausbreiten, wenn Abfall in der Nähe von Wohngebieten oder öffentlichen Einrichtungen aufbewahrt wird. Es ist wichtig, dass jeder Verantwortung übernimmt und Müll ordnungsgemäß entsorgt, um Littering und seine negativen Auswirkungen auf unsere Umwelt zu reduzieren.</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ArrowRightOnRectangleIcon } from "@heroicons/vue/24/solid";
+import { ChevronDownIcon } from "@heroicons/vue/24/solid";
 </script>
