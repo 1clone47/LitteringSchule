5bb3e1ef809acf53734e47bd7b737b4ff8a449fa
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-10-05 22:39:24 +0200

    add content to measures

--- components/Measures/index.vue
+++ components/Measures/index.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="py-24 px-8 lg:px-32 xl:px-64 flex flex-col items-center">
-    <p class="font-bold text-3xl flex justify-center">
-      Was können wir tun?
+  <div class="lg:mt-12 px-8 lg:px-32 flex flex-col items-center">
+    <p class="font-bold text-3xl xl:text-[80px] lg:mb-3">
+      Maßnahmen
     </p>
-    <p class="mt-4 mb-4 font-mono">
+  </div>
+  <div class="mt-6 bg-[#D9D9D9] px-24 text-2xl py-12">
+    <p>
       Die erfolgreiche Bekämpfung von Littering erfordert eine Kombination dieser Maßnahmen und eine kontinuierliche Anstrengung auf individueller, gemeinschaftlicher und gesellschaftlicher Ebene. Die Sensibilisierung der Menschen für die Konsequenzen von Littering und die Förderung verantwortungsvollen Verhaltens sind Schlüsselaspekte im Kampf gegen dieses Umweltproblem.
     </p>
-    <div v-for="measure in MEASURES" class="mt-8 flex lg:flex-row flex-col">
-      <img :src="measure.image" class="rounded-lg shadow-xl lg:h-[450px] lg:w-[450px] h-[250px] w-full mb-4" alt="">
-      <div class="flex flex-col items-center justify-center px-4">
-        <p class="text-xl font-semibold">
-          {{ measure.title }}
-        </p>
-        <p class="font-mono mt-2">
-          {{ measure.content }}
-        </p>
+  </div>
+  <div class="lg:mt-12 px-8 lg:px-32 py-8 flex flex-col items-center">
+    <div v-for="item in MEASURES" class="flex flex-row mb-4 mt-4">
+      <div class="flex-1">
+        <img :src="item.image" alt="" class="rounded-lg shadow-lg">
+      </div>
+      <div class="flex flex-col flex-1 justify-center px-4">
+        <p class="text-[30px] font-semibold">{{ item.title }}</p>
+        <p class="text-lg">{{ item.content }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { MEASURES } from "~/components/Measures/measures-item";
-
-useSeoMeta({
-  title: 'Maßnahmen | Littering'
-})
+import { MEASURES } from "./measures-item";
 </script>
