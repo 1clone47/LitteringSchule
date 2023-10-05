d6c406a54c0e10a3a6c9b5703eb26b256afc32e1
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-10-05 21:41:38 +0200

    add content to causes

--- components/Causes/index.vue
+++ components/Causes/index.vue
@@ -9,8 +9,19 @@
       Die Bekämpfung von Littering erfordert eine umfassende Herangehensweise, die Bildung, Bewusstseinsbildung, angemessene Infrastruktur, rechtliche Durchsetzung und kulturelle Veränderungen umfasst. Nur wenn diese Ursachen angegangen werden, kann eine langfristige Verringerung von Littering erreicht werden.
     </p>
   </div>
+  <div class="lg:mt-12 px-8 lg:px-32 py-8 flex flex-col items-center">
+    <div v-for="item in REASONS" class="flex flex-row mb-4 mt-4">
+      <div class="flex-1">
+        <img :src="item.image" alt="" class="rounded-lg shadow-lg">
+      </div>
+      <div class="flex flex-col flex-1 justify-center px-4">
+        <p class="text-[30px] font-semibold">{{ item.title }}</p>
+        <p class="text-lg">{{ item.content }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+import { REASONS } from './causes-items'
 </script>
