9a9b660e1d0c9c282f5748d6dc06a6411b1910f6
1clone47 <91965237+1clone47@users.noreply.github.com>
2023-10-05 21:24:23 +0200

    add banner to causes

--- .nuxt/app.config.mjs
+++ .nuxt/app.config.mjs
@@ -1,6 +1,6 @@
 
 import { updateAppConfig } from '#app'
-import { defuFn } from 'S:/Übungen/LitteringSchule/LitteringSchule/node_modules/defu/dist/defu.mjs'
+import { defuFn } from '/Users/andy/code/web/LitteringSchule/node_modules/defu/dist/defu.mjs'
 
 const inlineConfig = {}
 
--- components/Causes/index.vue
+++ components/Causes/index.vue
@@ -1,29 +1,16 @@
 <template>
-  <div class="py-24 px-8 lg:px-32 xl:px-64 flex flex-col items-center">
-    <p class="font-bold text-3xl">
-      Was sind die Ursachen?
+  <div class="lg:mt-12 px-8 lg:px-32 flex flex-col items-center">
+    <p class="font-bold text-3xl xl:text-[80px] lg:mb-3">
+      Ursachen
     </p>
-    <p class="mt-4 mb-4 font-mono">
+  </div>
+  <div class="mt-6 bg-[#D9D9D9] px-24 text-2xl py-12">
+    <p>
       Die Bekämpfung von Littering erfordert eine umfassende Herangehensweise, die Bildung, Bewusstseinsbildung, angemessene Infrastruktur, rechtliche Durchsetzung und kulturelle Veränderungen umfasst. Nur wenn diese Ursachen angegangen werden, kann eine langfristige Verringerung von Littering erreicht werden.
     </p>
-    <div v-for="reason in REASONS" class="mt-8 flex lg:flex-row flex-col">
-      <img :src="reason.image" class="rounded-lg shadow-xl lg:h-[450px] lg:w-[450px] h-[250px] w-full mb-4" alt="">
-      <div class="flex flex-col items-center justify-center px-4">
-        <p class="text-xl font-semibold">
-          {{ reason.title }}
-        </p>
-        <p class="font-mono mt-2">
-          {{ reason.content }}
-        </p>
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
-import { REASONS } from '~/components/Causes/causes-items'
 
-useSeoMeta({
-  title: 'Ursachen | Littering'
-})
 </script>
